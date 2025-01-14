c0b899a05c3764c0943eedb734615c56a2d3e76c
Fernando Monereo <55577139+Kao713@users.noreply.github.com>
2025-01-14 10:19:27 +0100

    touchscreen

--- Assets/Scripts/ControlLanzamiento.cs
+++ Assets/Scripts/ControlLanzamiento.cs
@@ -5,24 +5,57 @@ using UnityEngine.InputSystem;
 
 public class ControlLanzamiento : MonoBehaviour
 {
+    private Camera cam;
 
+    public GameObject bola;
+    private Rigidbody2D bolaRB;
+    private SpringJoint2D bolaSpringJoint;
+    public Rigidbody2D pivote;
+
+    private bool arrastrando;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
+        bolaRB = bola.GetComponent<Rigidbody2D>();
+        bolaSpringJoint = bola.GetComponent<SpringJoint2D>();
+        bolaSpringJoint.connectedBody = pivote;
+        arrastrando = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (bolaRB != null) return;
+
         //Se ha presionado?
-        if(!Touchscreen.current.primaryTouch.press.isPressed)
+        if (!Touchscreen.current.primaryTouch.press.isPressed && arrastrando == false)
         {
             return;
         }
-        Debug.Log("Presionado");
+        if (!Touchscreen.current.primaryTouch.press.isPressed && arrastrando == true)
+        {
+            arrastrando = false;
+            Invoke("LanzaBola", 1.0f);
+        }
+
+        if (Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            arrastrando = true;
+        }
 
         //Donde se ha pulsado
         Vector2 posicion = Touchscreen.current.primaryTouch.position.ReadValue();
+        //Debug.Log("posicion" + posicion + "*");
+        Vector3 posicionGlobal = cam.ScreenToWorldPoint(posicion);
+
+        //Control bola y pivote
+        bolaRB.position = posicionGlobal;
+
+    }
+
+    private void LanzaBola()
+    {
+        Debug.Log("lanza bola");
     }
 }
